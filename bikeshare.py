7497de2d27652a0dbd1de856e1c793395625255f
Mostafalce <61316717+Mostafalce@users.noreply.github.com>
2020-02-29 03:51:49 +0200

    make dictionary for data in 3 cities

---
+++ bikeshare.py
@@ -0,0 +1,36 @@
+import time
+import pandas as pd
+import numpy as np
+
+CITY_DATA = {'chicago': 'chicago.csv',
+              'new york city': 'new_york_city.csv',
+              'washington': 'washington.csv'}
+# make dictionary to data in 3 cities
+
+def check_input(input_str,input_type):
+    """
+    check the validity of user input.
+    input_str: is the input of the user
+    input_type: is the type of input: 1 = city, 2 = month, 3 = day
+    """
+    while True:
+        input_read=input(input_str)
+        try:
+            if input_read in ['chicago','new york city','washington'] and input_type == 1:
+                break
+            elif input_read in ['january', 'february', 'march', 'april', 'may', 'june','all'] and input_type == 2:
+                break
+            elif input_read in ['sunday','monday','tuesday','wednesday','thursday','friday','saturday','all'] and input_type == 3:
+                break
+            else:
+                if input_type == 1:
+                    print("Sorry, your input should be: chicago new york city or washington")
+                if input_type == 2:
+                    print("Sorry, your input should be: january, february, march, april, may, june or all")
+                if input_type == 3:
+                    print("Sorry, your input should be: sunday, ... friday, saturday or all")
+        except ValueError:
+            print("Sorry, your input is wrong")
+    return input_read
+
+def get_filters():
